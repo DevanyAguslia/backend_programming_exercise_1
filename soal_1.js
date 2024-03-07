e7f5f571d4ca506ec206bcec7094bebbb2e88336
Devany <devanyagusliaa@gmail.com>
2024-03-07 16:39:07 +0700

    535230189 Devany Aguslia

--- soal_1.js
+++ soal_1.js
@@ -1,31 +1,26 @@
-//535230189 Devany Aguslia
-//Buatlah program yang menampilkan semua bilangan prima dari angka 1 hingga 1,000
-
-function isPrime(num) {
-    if (num < 2) {
-        return false;
-    }
-    for (let i = 2; i <= Math.sqrt(num); i++) {
-        if (num % i === 0) {
+function prima(angka) {
+    for (let i = 2; i < angka; i++) {
+        if (angka % i === 0) {
             return false;
         }
     }
-    return true;
+    return angka > 1;
 }
 
-function displayPrimes(limit) {
-    const primes = [];
+function bilanganPrima(limit) {
+    const bilanganPrimaList = [];
+
     for (let i = 2; i <= limit; i++) {
-        if (isPrime(i)) {
-            primes.push(i);
+        if (prima(i)) {
+            bilanganPrimaList.push(i);
         }
     }
-    return primes;
+
+    return bilanganPrimaList;
 }
 
 const limit = 1000;
-const primeNumbers = displayPrimes(limit);
+const nomorBilanganPrima = bilanganPrima(limit);
 
-console.log(`All prime numbers from 1 to ${limit} are:`);
-console.log(primeNumbers);
-
+console.log(`menampilkan semua bilangan prima dari angka 1 hingga 1,000`);
+console.log(nomorBilanganPrima);
